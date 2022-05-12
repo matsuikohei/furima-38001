87d4280b405992c8ed1438ab78b40b1ac2a3b340
matsuikohei <kmatsui0612@gmail.com>
2022-05-12 14:56:58 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]\z/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]\z/ }
-  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶ]\z/ }
-  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶ]\z/ }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :kana_first_name
+    validates :kana_last_name  
+  end
   validates :birthday, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
