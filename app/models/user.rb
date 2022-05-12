50c170bb287d275e68cd0e62585289b425922243
matsuikohei <kmatsui0612@gmail.com>
2022-05-12 13:13:17 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]\z/ }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶ]\z/ }
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶ]\z/ }
+  validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
