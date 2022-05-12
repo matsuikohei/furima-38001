4df78f25234c673dbf91aaae4197a569051f5f69
matsuikohei <kmatsui0612@gmail.com>
2022-05-13 08:42:44 +0900

    商品情報の新規登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,11 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    Item.create(item_parameters)
   end
 
+  private
+  def item_parameters
+    params.require(:item).permit(:name, :text, :category_id, :status_id, :charge_id, :area_id, :delivery_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
