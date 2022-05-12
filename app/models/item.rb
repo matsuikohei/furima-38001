ebcae9e01f12ebdbbf3a20965cde4eb081301fe1
matsuikohei <kmatsui0612@gmail.com>
2022-05-12 18:02:09 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 }
+    validates :category_id
+    validates :status_id
+    validates :charge_id
+    validates :area_id
+    validates :days_id
+  end
+
+  validates :user, presence: true
+
 end
