50b7a98666fd8b004b04a9ca82465339262bd309
matsuikohei <kmatsui0612@gmail.com>
2022-05-13 15:50:09 +0900

    コードレビューご指摘項目の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -100,6 +100,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is invalid')
       end
+
+      it 'userが紐づいてないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
