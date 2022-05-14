cc5bc6437835d26bbee91699d642129b813fd147
matsuikohei <kmatsui0612@gmail.com>
2022-05-15 02:06:25 +0900

    サーバーサイドのトークンの受け取り

--- app/controllers/sold_items_controller.rb
+++ app/controllers/sold_items_controller.rb
@@ -7,6 +7,7 @@ class SoldItemsController < ApplicationController
   def create
     @sold_item_shipping = SoldItemShipping.new(sold_item_shipping_parameters)
     if @sold_item_shipping.valid?
+      pay_item
       @sold_item_shipping.save
       redirect_to root_path
     else
@@ -17,7 +18,16 @@ class SoldItemsController < ApplicationController
   
   private
   def sold_item_shipping_parameters
-    params.require(:sold_item_shipping).permit(:postal, :area_id, :city, :address, :building, :phone, :sold_item_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    item = Item.find(params[:item_id])
+    params.require(:sold_item_shipping).permit(:postal, :area_id, :city, :address, :building, :phone, :sold_item_id).merge(user_id: current_user.id, item_id: params[:item_id], price: item.price, token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = "sk_test_513c967a4409c5df3182a53d"
+    Payjp::Charge.create(
+      amount: sold_item_shipping_parameters[:price],
+      card: sold_item_shipping_parameters[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/sold_item_shipping.rb
+++ app/models/sold_item_shipping.rb
@@ -1,7 +1,6 @@
 class SoldItemShipping
   include ActiveModel::Model
-  attr_accessor :postal, :area_id, :city, :address, :building, :phone, :sold_item_id, :user_id, :item_id
-
+  attr_accessor :postal, :area_id, :city, :address, :building, :phone, :sold_item_id, :user_id, :item_id, :price, :token
   with_options presence: true do
     validates :postal, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :area_id, numericality: { other_than: 1 }
