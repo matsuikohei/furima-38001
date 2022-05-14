580d9e5e04195daf1b6af1f0a7773f575ed7eec9
matsuikohei <kmatsui0612@gmail.com>
2022-05-15 00:23:33 +0900

    商品購入画面の修正

--- app/controllers/sold_items_controller.rb
+++ app/controllers/sold_items_controller.rb
@@ -1,6 +1,16 @@
 class SoldItemsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @sold_item = SoldItem.new
+    @sold_item_shipping = SoldItemShipping.new
   end
+
+  def create
+    @sold_item_shipping = SoldItemShipping.new(sold_item_shipping_parameters)
+  end
+  
+  private
+  def sold_item_shipping_parameters
+    params.require(:sold_item_shipping).permit(:postal, :area_id, :city, :address, :building, :phone, :sold_item_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
